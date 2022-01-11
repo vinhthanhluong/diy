635f66bbe322ebc1bb7a73dc78276e6feb443f8a
thành <vinhthanhluong@gmail.com>
2022-01-11 18:09:31 +0700

    update page all  not rps

--- template/js/modules/FileActiveModule.js
+++ template/js/modules/FileActiveModule.js
@@ -1,5 +1,5 @@
 export default function FileActiveModule() {
-  
+
 
     $('.tab-link-mobi').on('click', function () {
         $('.tab-link').slideToggle();
@@ -11,5 +11,44 @@ export default function FileActiveModule() {
             $(this).next('.ques-cont').slideToggle();
         });
     }
+
+    if ($('.bx-video').length) {
+        $('.bx-video').on('click', function () {
+            $(this).toggleClass('active');
+        });
+    }
+
+    if ($('.vd-catalog').length) {
+        const $main = $('.vd-catalog');
+
+        $(document).mouseup(function (e) {
+            if (!$main.is(e.target) && $main.has(e.target).length === 0) {
+                $main.removeClass('active');
+            }
+        });
+
+        $('.vd-title').on('click', function () {
+            $main.toggleClass('active');
+        });
+        
+        $('.v-title').on('click', function () {
+            $(this).parent('.vd-item').toggleClass('active');
+            $(this).closest('.vd-item').siblings().removeClass('active');
+        });
     
+    }
+
+    if ($('.header-user').length) {
+        const $main = $('.header-user');
+        $(document).mouseup(function (e) { 
+            if (!$main.is(e.target) && $main.has(e.target).length === 0) {
+                $main.removeClass('active');
+            }
+        });
+         
+        $('.user-title').on('click', function () {
+            $main.toggleClass('active')
+        });
+    }
+
 }
